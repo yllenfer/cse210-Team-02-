2dfe67158a5580f4c3ca774281635c1a9935e772
blacksmithMike <67400418+blacksmithMike@users.noreply.github.com>
2021-09-30 17:04:19 -0500

    Add comment

--- helloworld.py
+++ helloworld.py
@@ -12,6 +12,7 @@
 
 print('This is the our first hello world. ')
 print('Hi, this is me again')
+# Mike lines 15-18
 print("Mike was here 2021")
 print("Killing time as usual")
 print("something else")
