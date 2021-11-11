e95ce7a95be902d8b1d7b3fb9dc4eb724791f594
Yllen Fernandez <yllenfernandez@gmail.com>
2021-11-11 13:38:30 -0600

    game week08

--- cse210-student-rfk-master/cse210-student-rfk-master/rfk/game/action.py
+++ cse210-student-rfk-master/cse210-student-rfk-master/rfk/game/action.py
@@ -10,10 +10,9 @@ class Action:
     """
 
     def execute(self, cast):
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
         raise NotImplementedError("execute not implemented in superclass")
-
-    """Executes the action using the given actors.
-
-    Args:
-        cast (dict): The game actors {key: tag, value: list}.
-    """
